1680d5d6a331d454d4ebced6e0ed89cc48c08fb8
Raoul Wols <raoulwols@gmail.com>
2020-10-04 13:57:49 +0200

    Simplify snippets even more. KISS!

--- tools/parse-snippets.py
+++ tools/parse-snippets.py
@@ -6,20 +6,10 @@ def main():
     commands = subprocess.check_output(["cmake", "--help-command-list"]).decode("utf-8").splitlines()
     completions = []
     for command in commands:
-        if command in ("function", "macro"):
-            continue
         completions.append({
             "trigger": "{}\tbuiltin".format(command),
-            "contents": "{}($0)".format(command)
+            "contents": command
         })
-    completions.append({
-        "trigger": "function\tbuiltin",
-        "contents": "function(${1:name} ${2:arguments})\n\t${0:# function body}\nendfunction(${1:name})"
-    })
-    completions.append({
-        "trigger": "macro\tbuiltin",
-        "contents": "macro(${1:name} ${2:arguments})\n\t${0:# macro body}\nendmacro(${1:name})"
-    })
     with open("CMake.sublime-completions", "w") as fp:
         json.dump(
             {
