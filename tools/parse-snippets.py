523b5f580c553f0c3507afe76d1c6b0276fab66f
Raoul Wols <raoulwols@gmail.com>
2020-12-15 21:34:25 +0100

    Restore fancy snippets

--- tools/parse-snippets.py
+++ tools/parse-snippets.py
@@ -1,14 +1,25 @@
 import json
 import subprocess
+import os
 
 
 def main():
+    snippets = os.path.join(os.path.dirname(os.path.dirname(__file__)), "Snippets")
+    specials = set()
+    for file in os.listdir(snippets):
+        stem, _ = os.path.splitext(file)
+        stem = os.path.basename(stem)
+        print("found special command", stem)
+        specials.add(stem)
     commands = subprocess.check_output(["cmake", "--help-command-list"]).decode("utf-8").splitlines()
     completions = []
     for command in commands:
+        if command in specials:
+            print("skipping special command", command)
+            continue
         completions.append({
             "trigger": "{}\tbuiltin".format(command),
-            "contents": command
+            "contents": "{}($0)".format(command)
         })
     with open("CMake.sublime-completions", "w") as fp:
         json.dump(
